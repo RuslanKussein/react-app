55532d8f2627f9a05ab1afdcc95546aec27756c5
RuslanKussein <ruslankusein@gmail.com>
2020-06-19 23:21:54 +0500

    lifting state in react

--- src/App.js
+++ src/App.js
@@ -20,9 +20,17 @@ const App = () => {
         },
     ];
 
+    const [searchTerm, setSearchTerm] = React.useState('');
+
     const handleSearch = event => {
-        console.log(event.target.value);
+        setSearchTerm(event.target.value);
     };
+
+    const searchedStories = stories.filter(story => {
+        return story.title
+            .toLowerCase()
+            .includes(searchTerm.toLowerCase());
+    });
 
     return (
         <div>
@@ -32,37 +40,24 @@ const App = () => {
 
             <hr />
 
-            <List list={stories} />
+            <List list={searchedStories} />
         </div>
     );
 };
 
-const Search = props => {
-    const [searchTerm, setSearchTerm] = React.useState('');
-
-    const handleChange = event => {
-        setSearchTerm(event.target.value);
-
-        props.onSearch(event);
-    };
-
-    return (
-        <div>
-            <label htmlFor="search">Search: </label>
-            <input id="search" type="text" onChange={handleChange} />
-            <p>
-                Searching for <strong>{searchTerm}</strong>.
-            </p>
-        </div>
-    );
-};
+const Search = props => (
+    <div>
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text" onChange={props.onSearch} />
+    </div>
+);
 
 const List = props =>
     props.list.map(item => (
         <div key={item.objectID}>
-      <span>
-        <a href={item.url}>{item.title}</a>
-      </span>
+            <span>
+                <a href={item.url}>{item.title}</a>
+            </span>
             <span>{item.author}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
