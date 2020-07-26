89d179253138b1967449f315b2aea135cadc0ab3
RuslanKussein <ruslankusein@gmail.com>
2020-07-26 13:25:51 +0500

    explicit data fetching with react

--- src/App.js
+++ src/App.js
@@ -53,6 +53,10 @@ const App = () => {
         'React'
     );
 
+    const [url, setUrl] = React.useState(
+        `${API_ENDPOINT}${searchTerm}`
+    );
+
     const [stories, dispatchStories] = React.useReducer(
         storiesReducer,
         {data: [], isLoading: false, isError: false}
@@ -61,7 +65,7 @@ const App = () => {
     const handleFetchStories = React.useCallback(() => {
         if (!searchTerm) return;
         dispatchStories({ type: 'STORIES_FETCH_INIT' });
-        fetch(`${API_ENDPOINT}${searchTerm}`)
+        fetch(url)
             .then(response => response.json())
             .then(result => {
                 dispatchStories({
@@ -72,7 +76,7 @@ const App = () => {
             .catch(() =>
                 dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
             );
-    }, [searchTerm]);
+    }, [url]);
 
     React.useEffect(() => {
         handleFetchStories();
@@ -85,8 +89,12 @@ const App = () => {
         });
     };
 
-    const handleSearch = event => {
+    const handleSearchInput = event => {
         setSearchTerm(event.target.value);
+    };
+
+    const handleSearchSubmit = () => {
+        setUrl(`${API_ENDPOINT}${searchTerm}`);
     };
 
     return (
@@ -97,10 +105,19 @@ const App = () => {
                 id="search"
                 value={searchTerm}
                 isFocused
-                onInputChange={handleSearch}
+                onInputChange={handleSearchInput}
             >
                 <strong>Search:</strong>
             </InputWithLabel>
+
+            <button
+                type="button"
+                disabled={!searchTerm}
+                onClick={handleSearchSubmit}
+            >
+                Submit
+            </button>
+
 
             <hr />
 
